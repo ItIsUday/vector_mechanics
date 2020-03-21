8d2cee36beb27a54d04b67382ad292c32ce18448
Uday <iamuday1618@gmail.com>
2020-03-21 15:59:22 +0530

    Refactor to use operator overriding

--- vector.py
+++ vector.py
@@ -1,20 +1,34 @@
-import math
+from math import *
 
 
 class Vector:
 
     def __init__(self, mag, inclination):
         self.mag = mag
-        self.inclination = math.radians(inclination)
+        self.inclination = radians(inclination)
 
     def __str__(self):
-        return f'<mag = {self.mag}, inclination = {math.degrees(self.inclination)}>'
+        return f'Vector: <mag = {self.mag}, inclination = {degrees(self.inclination)}>'
 
     def __repr__(self):
-        return f'<mag = {self.mag}, inclination = {math.degrees(self.inclination)}>'
+        return f'Vector: <mag = {self.mag}, inclination = {degrees(self.inclination)}>'
+
+    def __add__(self, other):
+        h_comp_sum = self.horizontal_component().mag + other.horizontal_component().mag
+        v_comp_sum = self.vertical_component().mag + other.vertical_component().mag
+
+        res_mag = sqrt(h_comp_sum ** 2 + v_comp_sum ** 2)
+        res_inclination = degrees(atan(v_comp_sum / h_comp_sum))
+
+        return Vector(res_mag, res_inclination)
+
+    def __radd__(self, other):
+        if isinstance(other, Vector):
+            return self + other
+        return self
 
     def horizontal_component(self):
-        return Vector(self.mag * math.cos(self.inclination), 0)
+        return Vector(self.mag * cos(self.inclination), 0)
 
     def vertical_component(self):
-        return Vector(self.mag * math.sin(self.inclination), 90)
+        return Vector(self.mag * sin(self.inclination), 90)
